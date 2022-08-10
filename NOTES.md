5f575dec96d1d9684f5cab22ca88948ec3075d60
Jimmy <jimmy.hotchkiss@gmail.com>
2022-08-09 17:18:15 -0700

    Initial commit

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+* <script src="path" defer></script>
+    - defer - Is used when we place the script tag at the top of the html file and not at the closing </body> tag
+
+* CSS - Reset
+    - *::before and *::after captures the sudo elements where just using * does not
+
+* Stryker website - Has logo max-height of 47px
---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+const productElements = document.querySelectorAll('.product-text-chevron')
+
+
+productElements.forEach(productElement => {
+    productElement.addEventListener('click', (e) => {
+        rotateChevron(e)
+        openProductItems(e)
+       
+    })
+
+})
+
+function rotateChevron(e){
+    e.currentTarget.children[1].classList.toggle('rotate')
+}
+
+function openProductItems(e) {
+    console.log(e.currentTarget.nextElementSibling)
+
+}
