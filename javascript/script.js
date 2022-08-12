78d3dd13d51ce1edaaa030f4ea205f480f24064c
Jimmy <jimmy.hotchkiss@gmail.com>
2022-08-11 20:05:58 -0700

    Added some stying and button functionality.

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,29 @@
 const productElements = document.querySelectorAll('.product-text-chevron')
 const productItems = document.querySelectorAll('.product-items')
+const contentHomeDiv = document.querySelector('#content-home-id')
+
+// Content Home Div - Close all Product Items and Chevrons
+contentHomeDiv.addEventListener('click', () => {
+    closeAllChevrons()
+    CloseAllProductItems()
+} )
+
+function closeAllChevrons() {
+    productElements.forEach(productElement => {
+        console.log(productElement.children[1])
+        if (productElement.children[1].classList.contains('rotate')) {
+            productElement.children[1].classList.remove('rotate')
+        }
+    })
+}
+
+function CloseAllProductItems() {
+    productItems.forEach(productItem => {
+        if(productItem.classList.contains('open')) {
+            productItem.classList.remove('open')
+        }
+    })
+}
 
 
 productElements.forEach(productElement => {
